180a295cf28ba8933a0dc1737d0fba7dd56399a6
aires1294 <pedrotja.engenharia@gmail.com>
2023-06-16 18:10:40 -0300

    add listar-tarefa.html

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
   styleUrls: ['./listar-tarefa.component.css']
 })
-export class ListarTarefaComponent {
+export class ListarTarefaComponent implements OnInit {
+
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit() {
+      this.tarefas = this.listarTodos();
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
 
 }
